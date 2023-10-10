1f5a3332049f3a707413a1a8295ad8bb461372b2
Vitor Yuske <vitor.yuski1@gmail.com>
2023-10-10 14:29:05 -0400

    Memory implementation v_00

--- memory.vhdl
+++ memory.vhdl
@@ -1,7 +1,6 @@
--- Implementar para Versão 1
-
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity memory is
     generic (
@@ -14,9 +13,36 @@ entity memory is
         data_write: in std_logic; -- When '1', write data to memory
         -- Data address given to memory
         data_addr : in std_logic_vector(addr_width-1 downto 0);
-        -- Data sent from memory when data_read = '1' and data_write = '0'
+        -- Data sent to memory when data_read = '1' and data_write = '0'
         data_in : in std_logic_vector((data_width*2)-1 downto 0);
-        -- Data sent to memory when data_read = '0' and data_write = '1'
+        -- Data sent from memory when data_read = '0' and data_write = '1'
         data_out : out std_logic_vector((data_width*4)-1 downto 0)
     );
 end entity;
+
+architecture behavioral of memory is
+    subtype data_word is std_logic_vector(data_width-1 downto 0);
+    type data_memory is array (0 to (2**addr_width)-1) of data_word;
+    signal ram : data_memory := (others => (others => '0'));
+begin
+
+    -- Edge-triggered memory
+    process (clock)
+    begin
+        
+        if clock = '1' then
+            if data_read = '1' and data_write = '0' then
+                data_out <= ram(to_integer(unsigned(data_addr))+3) & ram(to_integer(unsigned(data_addr))+2) & ram(to_integer(unsigned(data_addr))+1) & ram(to_integer(unsigned(data_addr)));
+            elsif data_read = '0' and data_write = '1' then
+                ram(to_integer(unsigned(data_addr))) <= data_in(data_width-1 downto 0);
+                ram(to_integer(unsigned(data_addr))+1) <= data_in(2*data_width-1 downto data_width);
+            end if;
+
+        else
+            data_out <= std_logic_vector(to_unsigned(0, data_width*4));
+
+        end if;
+        
+    end process;
+
+end architecture;
---
+++ tb_memory.vhdl
@@ -0,0 +1,64 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity tb_memory is
+end;
+
+architecture hibrida of tb_memory is
+    constant addr_width : natural := 16;
+    constant data_width : natural := 8;
+    signal clock, data_read, data_write : std_logic;
+    signal data_addr : std_logic_vector(addr_width-1 downto 0);
+    signal data_in : std_logic_vector((data_width*2)-1 downto 0);
+    signal data_out : std_logic_vector((data_width*4)-1 downto 0);
+begin
+    mem :   entity work.memory(behavioral)
+            generic map (addr_width => addr_width, data_width => data_width)
+            port map (clock => clock, data_read => data_read, data_write => data_write, data_addr => data_addr, data_in => data_in, data_out => data_out);
+        
+    estimulo_checagem : process is
+        type colunas_tabela_verdade is record
+            clock, data_read, data_write : std_logic;
+            data_addr : std_logic_vector(addr_width-1 downto 0);
+            data_in : std_logic_vector((data_width*2)-1 downto 0);
+            data_out : std_logic_vector((data_width*4)-1 downto 0);
+        end record;
+
+        type vetor_tabela_verdade is array (0 to 11) of colunas_tabela_verdade;
+
+        constant tabela_verdade : vetor_tabela_verdade := (
+            ('0', '0', '1', x"0010", x"1234", x"00000000"),
+            ('1', '0', '1', x"0010", x"1234", x"00000000"),
+            ('0', '1', '0', x"0010", x"0000", x"00000000"),
+            ('1', '1', '0', x"0010", x"0000", x"00001234"),
+            ('0', '1', '0', x"000F", x"0000", x"00000000"),
+            ('1', '1', '0', x"000F", x"0000", x"00123400"),
+            ('0', '1', '0', x"000E", x"0000", x"00000000"),
+            ('1', '1', '0', x"000E", x"0000", x"12340000"),
+            ('0', '0', '1', x"000E", x"5566", x"00000000"),
+            ('1', '0', '1', x"000E", x"5566", x"00000000"),
+            ('0', '1', '0', x"000E", x"0000", x"00000000"),
+            ('1', '1', '0', x"000E", x"0000", x"12345566")
+        );
+
+    begin
+
+        for i in tabela_verdade'range loop
+            clock <= tabela_verdade(i).clock;
+            data_read <= tabela_verdade(i).data_read;
+            data_write <= tabela_verdade(i).data_write;
+            data_addr <= tabela_verdade(i).data_addr;
+            data_in <= tabela_verdade(i).data_in;
+
+            wait for 1 ns;
+
+            assert data_out = tabela_verdade(i).data_out report "ERROR: Wrong data_out value at teste case: " & integer'image(i);
+            
+        end loop; 
+
+        report "The end of tests" ;
+
+        wait;
+    end process estimulo_checagem;
+end;
